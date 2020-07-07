06245987187896834d7d3dbcaa7366c257df95f7
mithem <mithem@github.com>
2020-07-07 12:20:25 +0200

    Initial Commit

---
+++ WaypointHelper/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WaypointHelper
+//
+//  Created by Miguel Themann on 07.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
